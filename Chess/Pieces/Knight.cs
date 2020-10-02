5cd80e4f9f3ba4b7cb63bbc5d025a750b79fe8dd
enisfarizi <57398441+enisfarizi@users.noreply.github.com>
2020-10-02 09:09:57 +0200

    Knight added function

--- Chess/Pieces/Knight.cs
+++ Chess/Pieces/Knight.cs
@@ -4,9 +4,30 @@ using System.Linq;
 
 namespace Chess
 {
-	public class Knight
-	{
-		
-		
-	}
+    public class Knight : Piece
+    {
+        public static readonly List<Tuple<int, int>> KnightOffsets = new List<Tuple<int, int>>()
+        {
+			//Tuple<X-Offset-Value, Y-Offset-Value>
+			new Tuple<int, int>(-1,2),
+            new Tuple<int, int>(1,2),
+            new Tuple<int, int>(-2,1),
+            new Tuple<int, int>(2,1),
+            new Tuple<int, int>(-2,-1),
+            new Tuple<int, int>(2,-1),
+            new Tuple<int, int>(-1,-2),
+            new Tuple<int, int>(1,-2),
+        };
+
+        public Knight(EPieceColor color, PiecePos position, double value) : base(color, position, value)
+        {
+
+        }
+
+        public override List<PiecePos> PossibleMoves()
+        {
+            return OffsetMovementHandle(KnightOffsets, true);
+        }
+
+    }
 }
